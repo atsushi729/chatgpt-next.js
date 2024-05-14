33b8140b943c5f6d70c7787a0556b8a855a01fa5
Atsushi <atu729@icloud.com>
2024-05-14 18:58:47 +0900

    feat : api request

--- src/app/api/motivation/route.ts
+++ src/app/api/motivation/route.ts
@@ -1,17 +1,55 @@
 import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
 
+//---------------------------------------------------------------------
+//                            Configuration
+//---------------------------------------------------------------------
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+console.log(openai);
+
+//---------------------------------------------------------------------
+//                             API request
+//---------------------------------------------------------------------
 export function GET(
   request: NextRequest,
   response: NextResponse
 ): NextResponse {
-  // GET /api/users リクエストの処理
   return new NextResponse("GET request processed");
 }
 
-export function POST(
-  request: NextRequest,
-  response: NextResponse
-): NextResponse {
-  // POST /api/users リクエストの処理
-  return new NextResponse("POST request processed");
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  try {
+    const body = await request.json();
+    const prompt = body.prompt;
+
+    if (!prompt) {
+      return new NextResponse(JSON.stringify({ error: "Prompt is required" }), {
+        status: 400,
+      });
+    }
+
+    if (prompt.length > 100) {
+      return new NextResponse(JSON.stringify({ error: "Prompt is too long" }), {
+        status: 400,
+      });
+    }
+
+    const completion = await openai.completions.create({
+      model: "gpt-3.5-turbo",
+      prompt: `Create a motivational message for the following prompt: ${prompt}`,
+      max_tokens: 500,
+    });
+
+    const quote = completion.choices[0].text;
+    return new NextResponse(JSON.stringify({ quote }), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching quote from OpenAI:", error);
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to fetch quote from OpenAI" }),
+      { status: 500 }
+    );
+  }
 }
